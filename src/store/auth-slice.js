1d13578d764818c78a76582a177b599ca1b8f124
oussama ennadafy <oussama.ennadafy@gmail.com>
2023-05-28 13:36:48 +0100

    practice extra reducers

--- src/store/auth-slice.js
+++ src/store/auth-slice.js
@@ -1,30 +1,43 @@
 import { createSlice } from "@reduxjs/toolkit";
+import postsSlice from "./posts-slice";
 
 const initialState = {
   isLoggedIn: false,
   user: null,
+  posts: [],
 };
 
 const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    login(_, action)
-    {
+    login(state, action) {
       const { id, firstName, lastName } = action.payload;
-      if (id, firstName, lastName)
+      if ((id, firstName, lastName))
         return {
+          ...state,
           isLoggedIn: true,
           user: {
-            id, firstName, lastName
+            id,
+            firstName,
+            lastName,
           },
         };
       else return initialState;
     },
-    logout()
-    {
+    logout() {
       return initialState;
     },
+  },
+  // extraReducers: {
+  //   ["posts/addPost"]: (state, action) => {
+  //     state.posts.push(action.payload.post);
+  //   },
+  // },
+  extraReducers: (builder) => {
+    builder.addCase(postsSlice.actions.addPost, (state, action) => {
+      state.posts.push(action.payload.post);
+    });
   },
 });
 
